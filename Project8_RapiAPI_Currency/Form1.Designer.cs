42db8b898b3b926b6cb2017727f7050cc8f4394a
gokhanymn <gokhanyman@gmail.com>
2025-03-17 15:23:57 +0300

    Rapid API Kullanımı Döviz kuru Çekip Hesaplama

---
+++ Project8_RapiAPI_Currency/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Project8_RapiAPI_Currency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDolar = new System.Windows.Forms.Label();
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.rdbDolar = new System.Windows.Forms.RadioButton();
+            this.rdbEuro = new System.Windows.Forms.RadioButton();
+            this.rdbGbp = new System.Windows.Forms.RadioButton();
+            this.btnIslemYap = new System.Windows.Forms.Button();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSterlin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDolar
+            // 
+            this.lblDolar.AutoSize = true;
+            this.lblDolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDolar.Location = new System.Drawing.Point(66, 68);
+            this.lblDolar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDolar.Name = "lblDolar";
+            this.lblDolar.Size = new System.Drawing.Size(51, 20);
+            this.lblDolar.TabIndex = 0;
+            this.lblDolar.Text = "label1";
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEuro.Location = new System.Drawing.Point(153, 68);
+            this.lblEuro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(51, 20);
+            this.lblEuro.TabIndex = 2;
+            this.lblEuro.Text = "label4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(64, 166);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Birim Tutar:";
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(157, 163);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(124, 26);
+            this.txtUnitPrice.TabIndex = 5;
+            // 
+            // rdbDolar
+            // 
+            this.rdbDolar.AutoSize = true;
+            this.rdbDolar.Location = new System.Drawing.Point(52, 118);
+            this.rdbDolar.Name = "rdbDolar";
+            this.rdbDolar.Size = new System.Drawing.Size(65, 24);
+            this.rdbDolar.TabIndex = 6;
+            this.rdbDolar.TabStop = true;
+            this.rdbDolar.Text = "Dolar";
+            this.rdbDolar.UseVisualStyleBackColor = true;
+            // 
+            // rdbEuro
+            // 
+            this.rdbEuro.AutoSize = true;
+            this.rdbEuro.Location = new System.Drawing.Point(123, 118);
+            this.rdbEuro.Name = "rdbEuro";
+            this.rdbEuro.Size = new System.Drawing.Size(61, 24);
+            this.rdbEuro.TabIndex = 7;
+            this.rdbEuro.TabStop = true;
+            this.rdbEuro.Text = "Euro";
+            this.rdbEuro.UseVisualStyleBackColor = true;
+            // 
+            // rdbGbp
+            // 
+            this.rdbGbp.AutoSize = true;
+            this.rdbGbp.Location = new System.Drawing.Point(190, 118);
+            this.rdbGbp.Name = "rdbGbp";
+            this.rdbGbp.Size = new System.Drawing.Size(72, 24);
+            this.rdbGbp.TabIndex = 8;
+            this.rdbGbp.TabStop = true;
+            this.rdbGbp.Text = "Sterlin";
+            this.rdbGbp.UseVisualStyleBackColor = true;
+            // 
+            // btnIslemYap
+            // 
+            this.btnIslemYap.Location = new System.Drawing.Point(157, 236);
+            this.btnIslemYap.Name = "btnIslemYap";
+            this.btnIslemYap.Size = new System.Drawing.Size(124, 31);
+            this.btnIslemYap.TabIndex = 9;
+            this.btnIslemYap.Text = "İşlem Yap";
+            this.btnIslemYap.UseVisualStyleBackColor = true;
+            this.btnIslemYap.Click += new System.EventHandler(this.btnIslemYap_Click);
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(157, 197);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(124, 26);
+            this.txtTotalPrice.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(26, 200);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ödenecek Tutar:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(64, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Dolar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(153, 33);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Euro";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(238, 33);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Sterlin";
+            // 
+            // lblSterlin
+            // 
+            this.lblSterlin.AutoSize = true;
+            this.lblSterlin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSterlin.Location = new System.Drawing.Point(238, 68);
+            this.lblSterlin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSterlin.Name = "lblSterlin";
+            this.lblSterlin.Size = new System.Drawing.Size(51, 20);
+            this.lblSterlin.TabIndex = 14;
+            this.lblSterlin.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(339, 368);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblSterlin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnIslemYap);
+            this.Controls.Add(this.rdbGbp);
+            this.Controls.Add(this.rdbEuro);
+            this.Controls.Add(this.rdbDolar);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblEuro);
+            this.Controls.Add(this.lblDolar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDolar;
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.RadioButton rdbDolar;
+        private System.Windows.Forms.RadioButton rdbEuro;
+        private System.Windows.Forms.RadioButton rdbGbp;
+        private System.Windows.Forms.Button btnIslemYap;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSterlin;
+    }
+}
+
---
+++ Project8_RapiAPI_Currency/Form1.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project8_RapiAPI_Currency
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        decimal dolar =0 ;
+        decimal euro = 0;
+        decimal pound =0;
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            #region Dolar 
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=Usd&to=TRY&amount=1"),
+                Headers =
+    {
+        { "x-rapidapi-key", "c8f319f3abmshefcbcef1c1e1e7bp10e3afjsn3b0fda048e2e" },
+        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var json=JObject.Parse(body);
+                var value = json["result"].ToString();
+                lblDolar.Text = value;
+                dolar = decimal.Parse(value);
+                Console.WriteLine(body);
+
+            }
+
+            #endregion
+
+            #region Euro
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=EUR&to=TRY&amount=1"),
+                Headers =
+    {
+        { "x-rapidapi-key", "c8f319f3abmshefcbcef1c1e1e7bp10e3afjsn3b0fda048e2e" },
+        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                var json2 = JObject.Parse(body2);
+                var value2 = json2["result"].ToString();
+                lblEuro.Text = value2;
+                euro = decimal.Parse(value2);
+                Console.WriteLine(body2);
+
+            }
+            #endregion
+
+
+            #region Sterlin
+            var client3 = new HttpClient();
+            var request3 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=GBP&to=TRY&amount=1"),
+                Headers =
+    {
+        { "x-rapidapi-key", "c8f319f3abmshefcbcef1c1e1e7bp10e3afjsn3b0fda048e2e" },
+        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+    },
+            };
+            using (var response3 = await client3.SendAsync(request3))
+            {
+                response3.EnsureSuccessStatusCode();
+                var body3 = await response3.Content.ReadAsStringAsync();
+                var json3 = JObject.Parse(body3);
+                var value3 = json3["result"].ToString();
+                lblSterlin.Text = value3;
+                pound = decimal.Parse(value3);
+                Console.WriteLine(body3);
+
+            }
+            #endregion
+
+            txtTotalPrice.Enabled=false;
+        }
+
+        private void btnIslemYap_Click(object sender, EventArgs e)
+        {
+            decimal unitprice=decimal.Parse(txtUnitPrice.Text);
+      
+            decimal totalPrice = 0;
+
+            if (rdbDolar.Checked)
+            {
+                totalPrice = unitprice * dolar;
+            }
+            if (rdbEuro.Checked)
+            {
+                totalPrice = unitprice * euro;
+            }
+            if (rdbGbp.Checked)
+            {
+                totalPrice = unitprice * pound;
+            }
+            txtTotalPrice.Text = totalPrice.ToString();
+        }
+    }
+}
